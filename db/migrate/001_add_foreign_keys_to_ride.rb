46aaf7d2aca0d750278d5a412c1b7f3858497201
Dee Race <deerace12@gmail.com>
2018-06-18 21:19:44 +0000

    automatically backed up by learn

--- db/migrate/001_add_foreign_keys_to_ride.rb
+++ db/migrate/001_add_foreign_keys_to_ride.rb
@@ -1,6 +1,6 @@
 class AddForeignKeysToRide < ActiveRecord::Migration
   def change
-    add column :rides, :taxi_id, :integer
-    add column :rides, :passenger_id, :integer
+    add_column :rides, :taxi_id, :integer
+    add_column :rides, :passenger_id, :integer
   end
 end
